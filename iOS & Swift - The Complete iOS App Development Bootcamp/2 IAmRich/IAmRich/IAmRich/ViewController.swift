d71eb6f3a888a403bc28a1ef5f770e73e3704650
Jonathan Teo <91585955+JET2001@users.noreply.github.com>
2021-11-02 18:04:00 +0800

    Add files via upload

---
+++ iOS & Swift - The Complete iOS App Development Bootcamp/2 IAmRich/IAmRich/IAmRich/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Jon on 28/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // this is where most of your code will live!
+    // When you want to change the BEHAVIOUR of your app, then you're going to be writing some code here.
+    
+    // Notice that when we are in the code file, then the inspector tabs, they actually changes.
+    // when you're in a code file, then you can't change the colour of your code or the size of your code etc
+    // It does have other things related to the code files, such as the name of the code file / what type of file it is.
+    // The right hand pane is for changing certain properties of what's showing in the middle, while the left hand pane is mostly used for navigating between different files.
+    
+    // Now
+
+}
+
